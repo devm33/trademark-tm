25b956c21dda3ee302505a5f94e8eb965519cec6
devm33@gmail.com <devm33@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-13 22:21:13 +0000

    Empty World class

---
+++ trunk/Oregon Trail/src/game/World.java
@@ -0,0 +1,19 @@
+package game;
+
+/**
+ * The World class holds the overarching organization for the Oregon Trail Game
+ * 
+ * 
+ * @author Devraj Mehta
+ *
+ */
+public class World {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+
+}
