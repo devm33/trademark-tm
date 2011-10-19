7b39351ee07c85c13795571ce4b6ec2c2ebbc203
jaron.gao@gmail.com <jaron.gao@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-19 16:52:59 +0000

    added comments to config, connected labels to items in store

--- Oregon Trail/src/game/Store.java
+++ Oregon Trail/src/game/Store.java
@@ -15,6 +15,8 @@ import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class Store {
 
@@ -58,45 +60,35 @@ public class Store {
 		shlStoer.setText("Oregon Trail");
 		shlStoer.setLayout(new FormLayout());
 		
-		List list = new List(shlStoer, SWT.BORDER);
-		list.setItems(new String[] {"ammo", "food", "water"});
+		final List list = new List(shlStoer, SWT.BORDER);
+		list.setItems(new String[] {"Ammunition", "Food", "Water"});
 		FormData fd_list = new FormData();
 		fd_list.bottom = new FormAttachment(100, -23);
+		fd_list.top = new FormAttachment(0, 10);
 		fd_list.right = new FormAttachment(100, -283);
 		fd_list.left = new FormAttachment(0, 10);
 		list.setLayoutData(fd_list);
-		
-		Label lblNewLabel = new Label(shlStoer, SWT.NONE);
-		fd_list.top = new FormAttachment(lblNewLabel, 6);
-		lblNewLabel.setFont(SWTResourceManager.getFont("Tahoma", 10, SWT.NORMAL));
-		FormData fd_lblNewLabel = new FormData();
-		fd_lblNewLabel.top = new FormAttachment(0);
-		fd_lblNewLabel.left = new FormAttachment(0, 10);
-		fd_lblNewLabel.bottom = new FormAttachment(100, -258);
-		fd_lblNewLabel.right = new FormAttachment(100, -317);
-		lblNewLabel.setLayoutData(fd_lblNewLabel);
-		lblNewLabel.setText("Store Inventory");
 		
 		Label lbl1 = new Label(shlStoer, SWT.NONE);
 		FormData fd_lbl1 = new FormData();
 		fd_lbl1.top = new FormAttachment(list, 0, SWT.TOP);
 		fd_lbl1.left = new FormAttachment(list, 24);
 		lbl1.setLayoutData(fd_lbl1);
-		lbl1.setText("Name");
+		lbl1.setText("Name:");
 		
 		Label lbl2 = new Label(shlStoer, SWT.NONE);
 		FormData fd_lbl2 = new FormData();
 		fd_lbl2.top = new FormAttachment(lbl1, 7);
 		fd_lbl2.left = new FormAttachment(lbl1, 0, SWT.LEFT);
 		lbl2.setLayoutData(fd_lbl2);
-		lbl2.setText("Weight");
+		lbl2.setText("Weight:");
 		
 		Label lbl3 = new Label(shlStoer, SWT.NONE);
 		FormData fd_lbl3 = new FormData();
 		fd_lbl3.top = new FormAttachment(lbl2, 9);
 		fd_lbl3.left = new FormAttachment(lbl1, 0, SWT.LEFT);
 		lbl3.setLayoutData(fd_lbl3);
-		lbl3.setText("Price");
+		lbl3.setText("Price:");
 		
 		Label lbl4 = new Label(shlStoer, SWT.NONE);
 		FormData fd_lbl4 = new FormData();
@@ -105,35 +97,36 @@ public class Store {
 		lbl4.setLayoutData(fd_lbl4);
 		lbl4.setText("Description:");
 		
-		Label lblName = new Label(shlStoer, SWT.NONE);
+		final Label lblName = new Label(shlStoer, SWT.NONE);
 		FormData fd_lblName = new FormData();
 		fd_lblName.top = new FormAttachment(list, 0, SWT.TOP);
 		fd_lblName.left = new FormAttachment(lbl1, 6);
 		lblName.setLayoutData(fd_lblName);
-		lblName.setText("Sample");
+		lblName.setText("No Item Selected");
 		
-		Label lblWeight = new Label(shlStoer, SWT.NONE);
+		final Label lblWeight = new Label(shlStoer, SWT.NONE);
 		FormData fd_lblWeight = new FormData();
+		fd_lblWeight.right = new FormAttachment(lbl2, 41, SWT.RIGHT);
 		fd_lblWeight.bottom = new FormAttachment(lbl2, 0, SWT.BOTTOM);
 		fd_lblWeight.left = new FormAttachment(lbl2, 6);
 		lblWeight.setLayoutData(fd_lblWeight);
-		lblWeight.setText("1");
 		
-		Label lblPrice = new Label(shlStoer, SWT.NONE);
+		final Label lblPrice = new Label(shlStoer, SWT.NONE);
 		FormData fd_lblPrice = new FormData();
+		fd_lblPrice.right = new FormAttachment(lbl4, 0, SWT.RIGHT);
 		fd_lblPrice.bottom = new FormAttachment(lbl3, 0, SWT.BOTTOM);
 		fd_lblPrice.left = new FormAttachment(lbl3, 6);
 		lblPrice.setLayoutData(fd_lblPrice);
-		lblPrice.setText("2");
 		
-		Label lblDesc = new Label(shlStoer, SWT.NONE);
+		final Label lblDesc = new Label(shlStoer, SWT.WRAP);
+		lblDesc.setText("Select an item to buy from the store inventory.");
 		FormData fd_lblDesc = new FormData();
 		fd_lblDesc.top = new FormAttachment(lbl4, 0, SWT.TOP);
 		fd_lblDesc.left = new FormAttachment(lbl4, 6);
 		lblDesc.setLayoutData(fd_lblDesc);
-		lblDesc.setText("test");
 		
 		Button btnPurchase = new Button(shlStoer, SWT.NONE);
+		
 		FormData fd_btnPurchase = new FormData();
 		fd_btnPurchase.bottom = new FormAttachment(100, -58);
 		fd_btnPurchase.left = new FormAttachment(list, 61);
@@ -141,6 +134,7 @@ public class Store {
 		btnPurchase.setText("Purchase");
 		
 		Button btnExitStore = new Button(shlStoer, SWT.NONE);
+		fd_lblDesc.right = new FormAttachment(btnExitStore, 0, SWT.RIGHT);
 		FormData fd_btnExitStore = new FormData();
 		fd_btnExitStore.top = new FormAttachment(btnPurchase, 0, SWT.TOP);
 		fd_btnExitStore.left = new FormAttachment(btnPurchase, 40);
@@ -148,6 +142,8 @@ public class Store {
 		btnExitStore.setText("Exit Store");
 		
 		txtAmount = new Text(shlStoer, SWT.BORDER);
+		fd_lblDesc.bottom = new FormAttachment(txtAmount, -6);
+		fd_lblName.right = new FormAttachment(txtAmount, 0, SWT.RIGHT);
 		FormData fd_txtAmount = new FormData();
 		fd_txtAmount.bottom = new FormAttachment(btnPurchase, -18);
 		fd_txtAmount.left = new FormAttachment(list, 90);
@@ -158,7 +154,46 @@ public class Store {
 		fd_lblAmount.top = new FormAttachment(txtAmount, 3, SWT.TOP);
 		fd_lblAmount.right = new FormAttachment(lbl4, 0, SWT.RIGHT);
 		lblAmount.setLayoutData(fd_lblAmount);
-		lblAmount.setText("Amount");
+		lblAmount.setText("Amount:");
 
+		//When user selects an item in the store inventory list.
+		list.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				lblName.setText(list.getItem(list.getSelectionIndex()));
+				/*need to hold weight, price, and description in some database*/
+				
+				if(list.getSelectionIndex()==0){
+					//If Ammunition is selected in the list
+					lblWeight.setText("5 lbs");
+					lblPrice.setText("$20");
+					lblDesc.setText("Hunting rifle rounds used for big game.");
+				} else if(list.getSelectionIndex()==1){
+					//If Food is selected in the list
+					lblWeight.setText("4 lbs");
+					lblPrice.setText("$10");
+					lblDesc.setText("Durable canned foodstuff.");
+				} else if(list.getSelectionIndex()==2){
+					//If Water is selected in the list
+					lblWeight.setText("10 lbs");
+					lblPrice.setText("$5");
+					lblDesc.setText("A bucket of water.");
+				}
+			}
+		});
+		
+		//When user clicks the purchase button.
+		btnPurchase.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		
+		//When user clicks the exit store button
+		btnExitStore.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
 	}
 }
