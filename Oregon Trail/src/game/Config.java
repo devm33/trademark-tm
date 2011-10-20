5eab80a97ad8c5617fb29ce5d22c16785f7a8d7b
jaron.gao@gmail.com <jaron.gao@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-20 22:50:50 +0000

    Converted Config from SWT Application Window to SWT Composite

--- Oregon Trail/src/game/Config.java
+++ Oregon Trail/src/game/Config.java
@@ -1,6 +1,7 @@
 package game;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -23,9 +24,7 @@ import org.eclipse.swt.events.MouseEvent;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Config {
-
-	protected Shell shlOregontrail;
+public class Config extends Composite{
 	private Text txtName4;
 	private Text txtLeaderName;
 	private Text txtName3;
@@ -43,140 +42,91 @@ public class Config {
 	private Traveler party4;
 	private Wagon wagon;
 	private List<Traveler> memberList;
-	
-	/**
-	 * Method to be called from world to start game
-	 * 
-	 * @return wagon the wagon that was created using config
-	 */
-	public Wagon startGame() {
-		try {
-			this.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return wagon;
-	}
-	
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			Config window = new Config();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shlOregontrail.open();
-		shlOregontrail.layout();
-		while (!shlOregontrail.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
-		display.close();
-	}
-	
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		
-		shlOregontrail = new Shell();
-		shlOregontrail.setSize(395, 293);
-		shlOregontrail.setText("Oregon Trail");
-		
-		txtLeaderName = new Text(shlOregontrail, SWT.BORDER);
+	public Config(Composite parent, int style) {
+		super(parent, style);
+
+		txtLeaderName = new Text(this, SWT.BORDER);
 		txtLeaderName.setText("Jonathan");
 		txtLeaderName.setBounds(71, 65, 76, 21);
 		
-		txtName1 = new Text(shlOregontrail, SWT.BORDER);
+		txtName1 = new Text(this, SWT.BORDER);
 		txtName1.setText("Wilson");
 		txtName1.setBounds(71, 90, 76, 21);
 		
-		txtName2 = new Text(shlOregontrail, SWT.BORDER);
+		txtName2 = new Text(this, SWT.BORDER);
 		txtName2.setText("Sarah");
 		txtName2.setBounds(71, 117, 76, 21);
 		
-		txtName3 = new Text(shlOregontrail, SWT.BORDER);
+		txtName3 = new Text(this, SWT.BORDER);
 		txtName3.setText("Sebastian");
 		txtName3.setBounds(71, 144, 76, 21);
 		
-		Label lblNewLabel = new Label(shlOregontrail, SWT.NONE);
+		Label lblNewLabel = new Label(this, SWT.NONE);
 		lblNewLabel.setAlignment(SWT.RIGHT);
 		lblNewLabel.setBounds(10, 68, 55, 15);
 		lblNewLabel.setText("Leader:");
 		
-		Label lblConfig = new Label(shlOregontrail, SWT.NONE);
+		Label lblConfig = new Label(this, SWT.NONE);
 		lblConfig.setFont(SWTResourceManager.getFont("Segoe UI", 18, SWT.BOLD));
 		lblConfig.setBounds(146, 10, 91, 32);
 		lblConfig.setText("CONFIG");
 		
-		txtName4 = new Text(shlOregontrail, SWT.BORDER);
+		txtName4 = new Text(this, SWT.BORDER);
 		txtName4.setText("Elizabeth");
 		txtName4.setBounds(71, 171, 76, 21);
 		
-		Label lblParty = new Label(shlOregontrail, SWT.NONE);
+		Label lblParty = new Label(this, SWT.NONE);
 		lblParty.setAlignment(SWT.RIGHT);
 		lblParty.setBounds(10, 90, 55, 15);
 		lblParty.setText("Party 1:");
 		
-		Label lblParty_3 = new Label(shlOregontrail, SWT.NONE);
+		Label lblParty_3 = new Label(this, SWT.NONE);
 		lblParty_3.setAlignment(SWT.RIGHT);
 		lblParty_3.setText("Party 2:");
 		lblParty_3.setBounds(10, 117, 55, 15);
 		
-		Label lblParty_2 = new Label(shlOregontrail, SWT.NONE);
+		Label lblParty_2 = new Label(this, SWT.NONE);
 		lblParty_2.setAlignment(SWT.RIGHT);
 		lblParty_2.setText("Party 3:");
 		lblParty_2.setBounds(10, 144, 55, 15);
 		
-		Label lblParty_1 = new Label(shlOregontrail, SWT.NONE);
+		Label lblParty_1 = new Label(this, SWT.NONE);
 		lblParty_1.setAlignment(SWT.RIGHT);
 		lblParty_1.setText("Party 4:");
 		lblParty_1.setBounds(10, 171, 55, 15);
 		
-		Label lblNewLabel_1 = new Label(shlOregontrail, SWT.NONE);
+		Label lblNewLabel_1 = new Label(this, SWT.NONE);
 		lblNewLabel_1.setAlignment(SWT.RIGHT);
 		lblNewLabel_1.setBounds(184, 68, 66, 15);
 		lblNewLabel_1.setText("Profession:");
 		
-		Label lblNewLabel_2 = new Label(shlOregontrail, SWT.NONE);
+		Label lblNewLabel_2 = new Label(this, SWT.NONE);
 		lblNewLabel_2.setAlignment(SWT.RIGHT);
 		lblNewLabel_2.setBounds(195, 120, 55, 15);
 		lblNewLabel_2.setText("Pace:");
 		
-		Label lblNewLabel_3 = new Label(shlOregontrail, SWT.NONE);
+		Label lblNewLabel_3 = new Label(this, SWT.NONE);
 		lblNewLabel_3.setAlignment(SWT.RIGHT);
 		lblNewLabel_3.setBounds(195, 174, 55, 15);
 		lblNewLabel_3.setText("Rations:");
 		
-		dropProfession = new Combo(shlOregontrail, SWT.READ_ONLY);
+		dropProfession = new Combo(this, SWT.READ_ONLY);
 		dropProfession.setItems(new String[] {"Farmer", "Banker", "Carpenter"});
 		dropProfession.setBounds(256, 65, 91, 23);
 		dropProfession.setText("Farmer");
 		
-		dropRations = new Combo(shlOregontrail, SWT.READ_ONLY);
+		dropRations = new Combo(this, SWT.READ_ONLY);
 		dropRations.setItems(new String[] {"Meager", "Normal", "Filling"});
 		dropRations.setBounds(256, 171, 91, 23);
 		dropRations.setText("Meager");
 		
-		dropPace = new Combo(shlOregontrail, SWT.READ_ONLY);
+		dropPace = new Combo(this, SWT.READ_ONLY);
 		dropPace.setItems(new String[] {"Slow", "Medium", "Fast"});
 		dropPace.setBounds(256, 117, 91, 23);
 		dropPace.setText("Slow");
 		
-		btnStart = new Button(shlOregontrail, SWT.NONE);
+		btnStart = new Button(this, SWT.NONE);
 		
 		//When user clicks the "Start Journey" button.
 		btnStart.addSelectionListener(new SelectionAdapter() {
@@ -264,12 +214,14 @@ public class Config {
 				System.out.println("Member3: " + party3.getName());
 				System.out.println("Member4: " + party4.getName());
 				
-				//close the window
-				shlOregontrail.dispose();
 			}
 		});
 		btnStart.setBounds(146, 220, 91, 25);
 		btnStart.setText("Start Journey");
-
+	}
+	
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
 	}
 }
