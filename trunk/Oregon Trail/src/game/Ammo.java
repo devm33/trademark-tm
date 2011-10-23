5acc0546b2e5c6c7a2ca88702f783d31819b37de
devm33@gmail.com <devm33@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-23 22:34:05 +0000

    Ammo class - should be working now

--- trunk/Oregon Trail/src/game/Ammo.java
+++ trunk/Oregon Trail/src/game/Ammo.java
@@ -10,19 +10,38 @@ package game;
 public class Ammo extends Item {
 	
 	/**
-	 * keeps track of the number of rounds in this ammount of ammo.
+	 * keeps track of the number of rounds in this amount of ammo.
 	 */
 	private int rounds;
+	/**
+	 * in order to keep track of rounds this class needs to know when
+	 * the number of ammunition is change. this var keeps track of what
+	 * it thinks the amount is.
+	 */
+	private int amount;
 	
 	/**
 	 * Create a new Ammo object. Weighs 3 lbs. Costs $2 for 20 rounds.
 	 */
 	public Ammo(){
 		super(3, 0, "Ammunition");
+		rounds = 0;
+		amount = 0;
 	}
+	
 	
 	@Override
 	public void use() {
+		if(amount != getNumber()) {
+			rounds += 20*(getNumber() - amount);
+			amount = getNumber();
+		}
+		if(rounds == 0)
+			return; //do nothing
+		if(rounds < 0) {
+			rounds = 0; //can't have negative amounts
+			return; //still do nothing
+		}
 		rounds--;
 		if(rounds % 20 == 0)
 			setNumber(getNumber()-1);
