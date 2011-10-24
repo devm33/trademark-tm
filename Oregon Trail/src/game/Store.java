52807c52d16add9d15c33650a6a29fa4c4194989
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-24 01:10:41 +0000

    tweaks to store and imports for storescreen

--- Oregon Trail/src/game/Store.java
+++ Oregon Trail/src/game/Store.java
@@ -1,10 +1,39 @@
 package game;
-
 /**
  * Backend for Store GUI
+ * 
+ * @author Stephen
  * @author Jaron
  *
  */
+import exceptions.InsufficentFundsException;
+import exceptions.WeightCapacityExceededException;
+import gui.MainScreen;
+import items.*;
+import items.Item;
+
 public class Store {
+	private String name;
+	
+	public Store(){
+		this.name = "default";
+		
+	}
+	
+	public void buy(Item i, int num, int price){
+		int buyNum = num;
+		int availCash = Integer.parseInt(MainScreen.getWagon().getCash());
+		int total = buyNum*price;
+		try{
+			MainScreen.getWagon().getLeader().setMoney(availCash-total);
+			MainScreen.getWagon().addToInventory(i, buyNum);
+		}
+		catch(InsufficentFundsException f){
+			
+		}
+		catch(WeightCapacityExceededException w){
+			
+		}
+	}
 
 }
