a0f3b549afb6c999c984dacc0b60dfce4225ada9
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-24 02:00:29 +0000

    store and inventory updates

--- trunk/Oregon Trail/src/game/Inventory.java
+++ trunk/Oregon Trail/src/game/Inventory.java
@@ -8,14 +8,16 @@ import items.Water;
 
 /**
  * Backend for Inventory GUI
+ * @author Stephen
  * @author Jaron
  *
  */
 public class Inventory {
 	private Item[]itemInventory;
 	private int length = 6;
-	
-	
+	/**
+	 * Constructor for empty game items in Inventory
+	 */
 	public Inventory(){
 		itemInventory = new Item[length];
 		itemInventory[0] = new Ammo();
@@ -23,38 +25,71 @@ public class Inventory {
 		itemInventory[2] = new Medicine();
 		itemInventory[3] = new Water();
 	}
-	
+	/**
+	 * Constructor for 3 specific items in Inventory
+	 * @param item0 an item to go in the Inventory
+	 * @param item1 an item to go in the Inventory
+	 * @param item2 an item to go in the Inventory
+	 */
 	public Inventory(Item item0,Item item1, Item item2){
 		itemInventory = new Item[length];
 		itemInventory[0] = item0;
 		itemInventory[1] = item1;
 		itemInventory[2] = item2;
 	}
-	
-	public Inventory(Item item0,Item item1, Item item2, Item item3, Item item4){
+	/**
+	 * Constructor for all items in Inventory
+	 * @param item0 an item to go in the Inventory
+	 * @param item1 an item to go in the Inventory
+	 * @param item2 an item to go in the Inventory
+	 * @param item3 an item to go in the Inventory
+	 * @param item4 an item to go in the Inventory
+	 * @param item5 an item to go in the Inventory
+	 */
+	public Inventory(Item item0,Item item1, Item item2, Item item3, Item item4, Item item5){
 		itemInventory = new Item[length];
 		itemInventory[0] = item0;
 		itemInventory[1] = item1;
 		itemInventory[2] = item2;
 		itemInventory[3] = item3;
 		itemInventory[4] = item4;
+		itemInventory[5] = item5;
 	}
+	/**
+	 * Method to check the length of Inventory
+	 * @return length of Inventory in int
+	 */
 	public int getLength(){
 		return length;
 	}
-	
+	/**
+	 * Method to return an Item
+	 * @param x the specified position in Inventory
+	 * @return itemInventory[x] the specified Item
+	 */
 	public Item getItem(int x){
 		return itemInventory[x];
 	}
-	
+	/**
+	 * Sets the number of an item in inventory
+	 * @param item the item to be adjusted
+	 * @param num the item's position in Inventory
+	 */
 	public void setItemNum(int item, int num){
 		itemInventory[item].setNumber(num);
 	}
-	
+	/**
+	 * Gets the itemInventory
+	 * @return itemInventory the array of Items
+	 */
 	public Item[] getItemInventory(){
 		return itemInventory;
 	}
-	
+	/**
+	 * Checks the contents of inventory for an item
+	 * @param i the item to check for
+	 * @return boolean
+	 */
 	public boolean contains(Item i) {
 		// TODO Auto-generated method stub
 		for (int x=0; x<length; x++){
