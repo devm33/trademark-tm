0be583e14022761e49fa16364e65cc2a226273f1
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-24 01:58:07 +0000

    updates to town

--- Oregon Trail/src/game/Town.java
+++ Oregon Trail/src/game/Town.java
@@ -3,25 +3,47 @@ package game;
 /**
  * backend for Town GUI
  * @author Jaron
+ * @author Stephen
  *
  */
 public class Town {
 	private Store townStore;
 	private String townName;
-	
+	/**
+	 * Constructs a town
+	 * @param townName String name of the town
+	 * @param townStore Store for the town
+	 */
 	public Town(String townName, Store townStore){
 		this.townName = townName;
 		this.townStore = townStore;
 	}
-	
+	/**
+	 * Constructor for a town that creates a store based on the town name
+	 * @param townName
+	 */
+	public Town(String townName){
+		this.townName = townName;
+		this.townStore = new Store(townName+" General Store");
+	}
+	/**
+	 * Default constructor for the town and store, Independence
+	 */
 	public Town(){
 		this.townName = "Indpendence";
 		this.townStore = new Store();
 	}
-	
+	/**
+	 * Method to return the store
+	 * @return townStore the store of this Town object
+	 */
 	public Store getStore(){
 		return townStore;
 	}
+	/**
+	 * Method to return the town Name
+	 * @return townName the name of the Town object
+	 */
 	public String getTownName(){
 		return townName;
 	}
