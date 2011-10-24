6237c906ed0400849cc443f4452e7980e3a86427
jaron.gao@gmail.com <jaron.gao@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-24 02:44:27 +0000

    exceptions have output

--- trunk/Oregon Trail/src/exceptions/WeightCapacityExceededException.java
+++ trunk/Oregon Trail/src/exceptions/WeightCapacityExceededException.java
@@ -11,5 +11,7 @@ package exceptions;
 public class WeightCapacityExceededException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-
+	public WeightCapacityExceededException(){
+		System.out.println("You have too much junk in your wagon trunk");
+	}
 }
