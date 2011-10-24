672ad5038c8bfab128293d40c7c4f6cc7b28721d
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-24 02:00:29 +0000

    store and inventory updates

--- Oregon Trail/src/game/Store.java
+++ Oregon Trail/src/game/Store.java
@@ -14,16 +14,26 @@ import items.Item;
 
 public class Store {
 	private String name;
-	
+	/**
+	 * constructor for the default store, Independence General Store
+	 */
 	public Store(){
 		this.name = "Independence General Store";
-		
 	}
+	/**
+	 * constructor for a store
+	 * @param name the name of the store
+	 */
 	public Store(String name){
 		this.name = name;
 		
 	}
-	
+	/**
+	 * the buy method for a given store
+	 * @param i the item to buy
+	 * @param num the number of that item to buy
+	 * @param price the price of the item
+	 */
 	public void buy(Item i, int num, int price){
 		int buyNum = num;
 		int availCash = Integer.parseInt(MainScreen.getWagon().getCash());
