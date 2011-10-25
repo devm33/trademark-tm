0e234bb58ac0b859ca3fdd755e00c128b6136cbc
dbyassmith@gmail.com <dbyassmith@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-25 23:26:55 +0000

    UI enhancements to WagonScreen (changed textfields to labels)

--- Oregon Trail/src/gui/WagonScreen.java
+++ Oregon Trail/src/gui/WagonScreen.java
@@ -12,6 +12,8 @@ import org.eclipse.swt.widgets.CoolBar;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.wb.swt.SWTResourceManager;
 /**
  * Screen to display the wagon contents and controls.
  * 
@@ -20,14 +22,9 @@ import org.eclipse.swt.events.SelectionEvent;
  *
  */
 public class WagonScreen extends Composite {
-	private Text txtThisIsYour;
-	private Text txtPace;
-	private Text txtRations;
 	private Text rationsDescript;
 	private Text paceDescript;
-	private Text txtFoodRemaining;
 	private Text food;
-	private Text txtDistanceRemaining;
 	private Text distance;
 	private Text leader;
 	private Text traveler0;
@@ -47,10 +44,6 @@ public class WagonScreen extends Composite {
 		
 		wagon = World.getWagon();
 		
-		txtThisIsYour = new Text(this, SWT.BORDER);
-		txtThisIsYour.setText("This is your wagon. There are many like it...");
-		txtThisIsYour.setBounds(0, 0, 115, 42);
-		
 		btnTakeTurn = new Button(this, SWT.NONE);
 		btnTakeTurn.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -63,21 +56,13 @@ public class WagonScreen extends Composite {
 				wagon.takeStep();
 			}
 		});
-		btnTakeTurn.setBounds(0, 49, 94, 28);
+		btnTakeTurn.setBounds(20, 37, 94, 28);
 		btnTakeTurn.setText("Take Turn");
 		
 		rations = new Combo(this, SWT.NONE);
 		rations.setItems(new String[] {"None", "bare-bones", "Meager", "Normal", "Wellfed"});
 		rations.setBounds(68, 83, 94, 22);
 		rations.setText("Normal");
-		
-		txtPace = new Text(this, SWT.BORDER);
-		txtPace.setText("Pace:");
-		txtPace.setBounds(0, 145, 64, 19);
-		
-		txtRations = new Text(this, SWT.BORDER);
-		txtRations.setText("Rations:");
-		txtRations.setBounds(0, 84, 64, 19);
 		
 		pace = new Combo(this, SWT.NONE);
 		pace.setItems(new String[] {"Stopped", "Leisurely", "Steady", "Grueling"});
@@ -90,16 +75,8 @@ public class WagonScreen extends Composite {
 		paceDescript = new Text(this, SWT.BORDER);
 		paceDescript.setBounds(174, 145, 210, 42);
 		
-		txtFoodRemaining = new Text(this, SWT.BORDER);
-		txtFoodRemaining.setText("Food remaining:");
-		txtFoodRemaining.setBounds(157, 10, 94, 19);
-		
 		food = new Text(this, SWT.BORDER);
 		food.setBounds(257, 10, 64, 19);
-		
-		txtDistanceRemaining = new Text(this, SWT.BORDER);
-		txtDistanceRemaining.setText("Distance remaining:");
-		txtDistanceRemaining.setBounds(157, 42, 115, 19);
 		
 		distance = new Text(this, SWT.BORDER);
 		distance.setBounds(278, 42, 64, 19);
@@ -118,6 +95,27 @@ public class WagonScreen extends Composite {
 		
 		traveler3 = new Text(this, SWT.BORDER);
 		traveler3.setBounds(219, 257, 200, 19);
+		
+		Label lblWagon = new Label(this, SWT.NONE);
+		lblWagon.setFont(SWTResourceManager.getFont("Tahoma", 14, SWT.NORMAL));
+		lblWagon.setBounds(10, 5, 75, 28);
+		lblWagon.setText("WAGON");
+		
+		Label lblFoodRemaing = new Label(this, SWT.NONE);
+		lblFoodRemaing.setBounds(163, 16, 88, 13);
+		lblFoodRemaing.setText("Food Remaing:");
+		
+		Label lblDistanceRemaing = new Label(this, SWT.NONE);
+		lblDistanceRemaing.setBounds(163, 45, 100, 13);
+		lblDistanceRemaing.setText("Distance Remaing:");
+		
+		Label lblRatiions = new Label(this, SWT.NONE);
+		lblRatiions.setBounds(13, 91, 49, 13);
+		lblRatiions.setText("Rations:");
+		
+		Label lblPace = new Label(this, SWT.NONE);
+		lblPace.setBounds(20, 147, 49, 13);
+		lblPace.setText("Pace:");
 	}
 	
 	public void update() {
