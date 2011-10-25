31d4f67c3861fc8428acb1127c2fba965aafaa46
devm33@gmail.com <devm33@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-25 22:29:27 +0000

    Blank wagon screen

---
+++ Oregon Trail/src/gui/WagonScreen.java
@@ -0,0 +1,19 @@
+package gui;
+
+import org.eclipse.swt.widgets.Composite;
+/**
+ * Screen to display the wagon contents and controls.
+ * 
+ * 
+ * @author Devraj Mehta
+ *
+ */
+public class WagonScreen extends Composite {
+
+	public WagonScreen(Composite arg0, int arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
