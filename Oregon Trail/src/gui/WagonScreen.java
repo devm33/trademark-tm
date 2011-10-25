313b957246c55129443016e5ea11b43986f8a7a6
devm33@gmail.com <devm33@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-25 23:20:11 +0000

    wagon stuff half filled out

--- Oregon Trail/src/gui/WagonScreen.java
+++ Oregon Trail/src/gui/WagonScreen.java
@@ -1,5 +1,8 @@
 package gui;
 
+import game.Wagon;
+import game.World;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
@@ -7,6 +10,8 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.widgets.CoolBar;
 import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 /**
  * Screen to display the wagon contents and controls.
  * 
@@ -29,19 +34,39 @@ public class WagonScreen extends Composite {
 	private Text traveler1;
 	private Text traveler2;
 	private Text traveler3;
+	private Button btnTakeTurn;
+	private Combo rations;
+	private Combo pace;
+	
+	
+	
+	private Wagon wagon;
 
 	public WagonScreen(Composite arg0, int arg1) {
 		super(arg0, arg1);
 		
+		wagon = World.getWagon();
+		
 		txtThisIsYour = new Text(this, SWT.BORDER);
-		txtThisIsYour.setText("This is your wagon");
+		txtThisIsYour.setText("This is your wagon. There are many like it...");
 		txtThisIsYour.setBounds(0, 0, 115, 42);
 		
-		Button btnTakeTurn = new Button(this, SWT.NONE);
+		btnTakeTurn = new Button(this, SWT.NONE);
+		btnTakeTurn.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				if(rations.get)
+				
+				
+				
+				
+				wagon.takeStep();
+			}
+		});
 		btnTakeTurn.setBounds(0, 49, 94, 28);
 		btnTakeTurn.setText("Take Turn");
 		
-		Combo rations = new Combo(this, SWT.NONE);
+		rations = new Combo(this, SWT.NONE);
 		rations.setItems(new String[] {"None", "bare-bones", "Meager", "Normal", "Wellfed"});
 		rations.setBounds(68, 83, 94, 22);
 		rations.setText("Normal");
@@ -54,7 +79,7 @@ public class WagonScreen extends Composite {
 		txtRations.setText("Rations:");
 		txtRations.setBounds(0, 84, 64, 19);
 		
-		Combo pace = new Combo(this, SWT.NONE);
+		pace = new Combo(this, SWT.NONE);
 		pace.setItems(new String[] {"Stopped", "Leisurely", "Steady", "Grueling"});
 		pace.setBounds(68, 144, 94, 22);
 		pace.setText("Steady");
@@ -93,6 +118,120 @@ public class WagonScreen extends Composite {
 		
 		traveler3 = new Text(this, SWT.BORDER);
 		traveler3.setBounds(219, 257, 200, 19);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public void update() {
+		
+		rations.setBounds(68, 83, 94, 22);
+		rations.setText("Normal");
+		
+		txtPace = new Text(this, SWT.BORDER);
+		txtPace.setText("Pace:");
+		txtPace.setBounds(0, 145, 64, 19);
+		
+		txtRations = new Text(this, SWT.BORDER);
+		txtRations.setText("Rations:");
+		txtRations.setBounds(0, 84, 64, 19);
+		
+		pace.setItems(new String[] {"Stopped", "Leisurely", "Steady", "Grueling"});
+		pace.setBounds(68, 144, 94, 22);
+		pace.setText("Steady");
+		
+		rationsDescript = new Text(this, SWT.BORDER);
+		rationsDescript.setBounds(174, 83, 210, 42);
+		
+		paceDescript = new Text(this, SWT.BORDER);
+		paceDescript.setBounds(174, 145, 210, 42);
+		
+		txtFoodRemaining = new Text(this, SWT.BORDER);
+		txtFoodRemaining.setText("Food remaining:");
+		txtFoodRemaining.setBounds(157, 10, 94, 19);
+		
+		food = new Text(this, SWT.BORDER);
+		food.setBounds(257, 10, 64, 19);
+		
+		txtDistanceRemaining = new Text(this, SWT.BORDER);
+		txtDistanceRemaining.setText("Distance remaining:");
+		txtDistanceRemaining.setBounds(157, 42, 115, 19);
+		
+		distance = new Text(this, SWT.BORDER);
+		distance.setBounds(278, 42, 64, 19);
+		
+		leader = new Text(this, SWT.BORDER);
+		leader.setBounds(0, 211, 200, 19);
+		
+		traveler0 = new Text(this, SWT.BORDER);
+		traveler0.setBounds(0, 241, 200, 19);
+		
+		traveler1 = new Text(this, SWT.BORDER);
+		traveler1.setBounds(0, 271, 200, 19);
+		
+		traveler2 = new Text(this, SWT.BORDER);
+		traveler2.setBounds(219, 224, 200, 19);
+		
+		traveler3 = new Text(this, SWT.BORDER);
+		traveler3.setBounds(219, 257, 200, 19);
+	}
+	
+	
+	private String rations(int r) {
+		switch(r) {
+		case 0:
+			return "None";
+		case 1:
+			return "Barebones";
+		case 2:
+			return "Meager";
+		case 3:
+			return "Normal";
+		case 4:
+			return "Wellfed";
+		}
+		return null;
+	}
+	private int rations(String r) {
+		switch(r) {
+		case 0:
+			return "None";
+		case 1:
+			return "Barebones";
+		case 2:
+			return "Meager";
+		case 3:
+			return "Normal";
+		case "Wellfed":
+			return 4;
+		}
+		reutnr -1;
+	}
+	private String pace(int p) {
+		switch(r) {
+		case 0:
+			return "None";
+		case 1:
+			return "Barebones";
+		case 2:
+			return "Meager";
+		case 3:
+			return "Normal";
+		case 4:
+			return "Wellfed";
+		}
+		return null;
+	}
+	private int pace(String p) {
+		switch(r) {
+		case 0:
+			return "None";
+		case 1:
+			return "Barebones";
+		case 2:
+			return "Meager";
+		case 3:
+			return "Normal";
+		case 4:
+			return "Wellfed";
+		}
+		return -1;
 	}
 }
