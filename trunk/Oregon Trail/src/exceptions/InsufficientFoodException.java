ada4c4926b220b38aabaa8af82f241739b40cbf1
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-10-25 22:45:16 +0000

    updated food and new InsufficientFoodException Method

---
+++ trunk/Oregon Trail/src/exceptions/InsufficientFoodException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+/**
+ * Class to throw when there is not enough food for the current ration setting.
+ * 
+ * @author Stephen Bentley
+ *
+ */
+
+public class InsufficientFoodException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	public InsufficientFoodException(){
+	}
+}
--- trunk/Oregon Trail/src/items/Food.java
+++ trunk/Oregon Trail/src/items/Food.java
@@ -1,21 +1,58 @@
 package items;
 
-public class Food extends Item {
+import exceptions.InsufficientFoodException;
+import game.World;
 
-	
-	
-	//from super
+public class Food extends Item {
+	//class needs adjustment for individual units of food.
+	/**
+	 * constructor for food
+	 */
 	public Food() {
 		super(5, 0, "Food");
 		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * Method for using food with input ration used
+	 * @param used the number of lbs of food used (increments of 5lbs) 
+	 */
+	public void use(int used) {
+		// TODO Auto-generated method stub
+		
+		try {
+			if (this.getNumber()>=used) {
+				if (this.getNumber() > 0) {
+					this.setNumber(this.getNumber() - used);
+				}
+			}
+			else{
+				throw new InsufficientFoodException();
+			}
+		} catch (InsufficientFoodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Method for using food with ration rate drawn from the wagon class
+	 */
 	@Override
 	public void use() {
-		// TODO Auto-generated method stub
-		if(this.getNumber()>0){
-			this.setNumber(this.getNumber()-1);
+		int used =World.getWagon().getRations();
+		try {
+			if (this.getNumber()>=used) {
+				if (this.getNumber() > 0) {
+					this.setNumber(this.getNumber() - used);
+				}
+			}
+			else{
+				throw new InsufficientFoodException();
+			}
+		} catch (InsufficientFoodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 	}
 
 }
