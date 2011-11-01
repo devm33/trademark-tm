77187cd098006fed21eb5b848182c380ac547a2f
jaron.gao@gmail.com <jaron.gao@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-11-01 23:45:20 +0000

    created FieldScreen, InnScreen, and WinScreen

---
+++ Oregon Trail/src/game/River.java
@@ -0,0 +1,13 @@
+package game;
+
+public class River {
+	private String name;
+	
+	public River(String name){
+		setName(name);
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
