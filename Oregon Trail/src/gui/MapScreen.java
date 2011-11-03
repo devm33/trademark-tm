7d73da0becdf0a100809e68af04330df3ae965b8
dbyassmith@gmail.com <dbyassmith@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-11-03 21:56:23 +0000

    Added top image to MapScreen

--- Oregon Trail/src/gui/MapScreen.java
+++ Oregon Trail/src/gui/MapScreen.java
@@ -1,5 +1,8 @@
 package gui;
 
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
@@ -7,14 +10,18 @@ import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Button;
 
+/**
+ * Screen which displays route Map
+ * 
+ * 
+ * @author - David Byas-Smith
+ */
+
 public class MapScreen extends Composite {
 
-	/**
-	 * Screen which displays route Map
-	 * 
-	 * 
-	 * @author - David Byas-Smith
-	 */
+
+	
+	private Image image;
 	public MapScreen(Composite parent, int style) {
 		super(parent, style);
 		
@@ -38,7 +45,15 @@ public class MapScreen extends Composite {
 		label.setText("0000");
 		
 		Canvas canvas = new Canvas(this, SWT.NONE);
+		image = new Image(getDisplay(),getClass().getResourceAsStream("images/wagon_Map.jpg"));
 		canvas.setBounds(10, 10, 430, 108);
+		
+		canvas.addPaintListener(new PaintListener() {
+	        public void paintControl(PaintEvent e) {
+	         e.gc.drawImage(image,0,0);
+	        }
+	    });
+		
 		
 		Canvas canvas_1 = new Canvas(this, SWT.NONE);
 		canvas_1.setBounds(10, 189, 430, 71);
