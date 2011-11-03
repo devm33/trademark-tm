302e22ef0104b4adb32b9bb3926bf4db3593aece
dbyassmith@gmail.com <dbyassmith@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-11-03 23:01:24 +0000

    added new progress bar and adjusted canvas. still needs to be hooked up.

--- Oregon Trail/src/gui/MapScreen.java
+++ Oregon Trail/src/gui/MapScreen.java
@@ -1,5 +1,7 @@
 package gui;
 
+import game.World;
+
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Image;
@@ -22,31 +24,47 @@ public class MapScreen extends Composite {
 
 	
 	private Image image;
+	
+	private int townDistanceRemaining;
+	private int gameDistanceRemaining;
+	private final int totalGameDistance = 1909;
+	
+	
 	public MapScreen(Composite parent, int style) {
 		super(parent, style);
 		
 		Label lblDest = new Label(this, SWT.NONE);
-		lblDest.setBounds(377, 127, 63, 13);
+		lblDest.setBounds(377, 170, 63, 13);
 		lblDest.setText("Ft. Kearny");
 		
-		Label lblIndependence = new Label(this, SWT.NONE);
-		lblIndependence.setBounds(10, 127, 82, 13);
-		lblIndependence.setText("Independence");
+		Label lbldepart = new Label(this, SWT.NONE);
+		lbldepart.setBounds(10, 170, 82, 13);
+		lbldepart.setText("Independence");
 		
-		ProgressBar progressBar = new ProgressBar(this, SWT.NONE);
-		progressBar.setBounds(10, 146, 430, 18);
 		
+		//townDistanceRemaining = Map.distanceToTown();
 		Label lblDistanceRemaining = new Label(this, SWT.NONE);
-		lblDistanceRemaining.setBounds(156, 170, 97, 13);
+		lblDistanceRemaining.setBounds(132, 266, 97, 13);
 		lblDistanceRemaining.setText("Distance Remaining:");
 		
-		Label label = new Label(this, SWT.NONE);
-		label.setBounds(259, 170, 49, 13);
-		label.setText("0000");
+		ProgressBar townProgressBar = new ProgressBar(this, SWT.NONE);
+		townProgressBar.setMaximum(10);
+		townProgressBar.setBounds(10, 189, 430, 18);
+		//townProgressBar.setMaximum();
+		
+		gameDistanceRemaining = totalGameDistance - World.getWagon().getDistance();
+		Label lblGameDistanceRemaining = new Label(this, SWT.NONE);
+		lblGameDistanceRemaining.setBounds(235, 266, 49, 13);
+		lblGameDistanceRemaining.setText(Integer.toString(gameDistanceRemaining));
+		
+		ProgressBar gameProgressBar = new ProgressBar(this, SWT.NONE);
+		gameProgressBar.setBounds(10, 242, 430, 18);
+		gameProgressBar.setMaximum(totalGameDistance);
+		
 		
 		Canvas canvas = new Canvas(this, SWT.NONE);
-		image = new Image(getDisplay(),getClass().getResourceAsStream("images/wagon_Map.jpg"));
-		canvas.setBounds(10, 10, 430, 108);
+		image = new Image(getDisplay(),getClass().getResourceAsStream("images/map1.jpg"));
+		canvas.setBounds(10, 10, 430, 154);
 		
 		canvas.addPaintListener(new PaintListener() {
 	        public void paintControl(PaintEvent e) {
@@ -54,13 +72,26 @@ public class MapScreen extends Composite {
 	        }
 	    });
 		
+		Button btnCloseMap = new Button(this, SWT.NONE);
+		btnCloseMap.setBounds(371, 267, 68, 23);
+		btnCloseMap.setText("Close Map");
 		
-		Canvas canvas_1 = new Canvas(this, SWT.NONE);
-		canvas_1.setBounds(10, 189, 430, 71);
 		
-		Button btnNewButton = new Button(this, SWT.NONE);
-		btnNewButton.setBounds(185, 267, 68, 23);
-		btnNewButton.setText("Close Map");
+		Label lblStart = new Label(this, SWT.NONE);
+		lblStart.setBounds(10, 223, 49, 13);
+		lblStart.setText("Start");
+		
+		Label lblOregon = new Label(this, SWT.NONE);
+		lblOregon.setBounds(391, 223, 49, 13);
+		lblOregon.setText("Oregon");
+		
+		Label label_1 = new Label(this, SWT.NONE);
+		label_1.setText("Distance Remaining:");
+		label_1.setBounds(132, 213, 97, 13);
+		
+		Label lblTownDistanceRemaining = new Label(this, SWT.NONE);
+		lblTownDistanceRemaining.setText("0000");
+		lblTownDistanceRemaining.setBounds(235, 213, 49, 13);
 
 	}
 
