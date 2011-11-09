465d0e3017d7f913dc48eb3e494335aaf0da0528
rheck360@gmail.com <rheck360@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-11-09 00:34:24 +0000

    Added parameters for depth and ferryCost and all related getters, setters, and constructors.

--- Oregon Trail/src/game/River.java
+++ Oregon Trail/src/game/River.java
@@ -7,6 +7,8 @@ package game;
  */
 public class River {
 	private String name;
+	private int depth;
+	private int ferryCost;
 	
 	/**
 	 * create a river with a name
@@ -14,6 +16,21 @@ public class River {
 	 */
 	public River(String name){
 		setName(name);
+		ferryCost = 25;
+		depth = 4;
+	}
+	
+	/**
+	 * create a river with a name, a depth, and a cost
+	 * @param name name of the river
+	 * @param depth depth of the river
+	 * @param cost cost of ferry to cross river
+	 */
+	public River(String name, int depth, int cost)
+	{
+		setName(name);
+		setDepth(depth);
+		setCost(cost);
 	}
 	
 	/**
@@ -30,6 +47,42 @@ public class River {
 	 */
 	public String getName() {
 		return name;
+	}
+	
+	/**
+	 * set river depth
+	 * @param d depth of the river
+	 */
+	public void setDepth(int d)
+	{
+		depth = d;
+	}
+	
+	/**
+	 * get river depth
+	 * @return depth of river
+	 */
+	public int getDepth()
+	{
+		return depth;
+	}
+	
+	/**
+	 * set cost of a ferry to cross river
+	 * @param c cost of the ferry
+	 */
+	public void setCost(int c)
+	{
+		ferryCost = c;
+	}
+	
+	/**
+	 * get cost of a ferry to cross river
+	 * @return the cost of the ferry
+	 */
+	public int getCost()
+	{
+		return ferryCost;
 	}
 	
 }
