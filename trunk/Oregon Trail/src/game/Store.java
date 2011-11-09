2fd120d94d96d83e829ed03eb191116c2c1d0930
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-11-09 00:11:17 +0000

    new store with updated inventory

--- trunk/Oregon Trail/src/game/Store.java
+++ trunk/Oregon Trail/src/game/Store.java
@@ -12,11 +12,28 @@ import items.Item;
 
 public class Store {
 	private String name;
+	private Inventory storeInventory;
+	private int[][] prices;
+	private int offset;
 	/**
 	 * constructor for the default store, Independence General Store
 	 */
 	public Store(){
 		this.setName("Independence General Store");
+		this.storeInventory = new Inventory();
+		for (int y=0;y<9;y++){
+			prices[y][0] = y;
+		}
+		prices[0][1] =2;
+		prices[1][1] =5;
+		prices[2][1] =10;
+		prices[3][1] =0;
+		prices[4][1] =10;
+		prices[5][1] =10;
+		prices[6][1] =40;
+		prices[7][1] =10;
+		prices[8][1] =10;
+
 	}
 	/**
 	 * constructor for a store
@@ -24,7 +41,21 @@ public class Store {
 	 */
 	public Store(String name){
 		this.setName(name);
-		
+		this.storeInventory = new Inventory();
+		for (int y=0;y<9;y++){
+			prices[y][0] = y;
+		}
+		prices[0][1] =(int) (2*(.5*offset));
+		prices[1][1] =(int) (5*(.5*offset));
+		prices[2][1] =10;
+		prices[3][1] =0;
+		prices[4][1] =10;
+		prices[5][1] =10;
+		prices[6][1] =40;
+		prices[7][1] =10;
+		prices[8][1] =10;
+
+	
 	}
 	/**
 	 * the buy method for a given store
@@ -49,7 +80,32 @@ public class Store {
 			throw new WeightCapacityExceededException();
 		}
 	}
-	
+	/**
+	 * the buy method for a given store
+	 * @param i the item to buy
+	 * @param num the number of that item to buy
+	 * @param price the price of the item
+	 */
+	public void buy(Item i, int num) throws InsufficientFundsException, WeightCapacityExceededException{
+		int newWeight = World.getWagon().getTotalWeight() + (num*i.getWeight());
+		int price = storeInventory.getPrice(i,getPrices());
+		
+		
+		if(newWeight < World.getWagon().getCapacity()){
+			int buyNum = num;
+			int availCash = Integer.parseInt(World.getWagon().getCash());
+			int total = buyNum*price;
+			try{
+				World.getWagon().getLeader().setMoney((availCash-total));
+				World.getWagon().addToInventory(i, buyNum);
+			}
+			catch(InsufficientFundsException f){
+				throw new InsufficientFundsException();
+			}
+		} else {
+			throw new WeightCapacityExceededException();
+		}
+	}
 	/**
 	 * setter for store name
 	 * @param name
@@ -67,13 +123,16 @@ public class Store {
 	}
 	
 	public void setInventory(int sum_distance) {
-		// TODO Auto-generated method stub
+		this.offset = (sum_distance/100);
 		
 	}
 	
 	public Inventory getInventory() {
 		//TODO
-		return null;
+		return storeInventory;
+	}
+	public int[][] getPrices(){
+		return prices;
 	}
 
 }
