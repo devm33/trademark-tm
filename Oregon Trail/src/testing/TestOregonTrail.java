e596420a9177ad729bb1a3514c97b5bc56219161
scbentley3@gmail.com <scbentley3@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-12-05 17:52:35 +0000

    store inventory testing method

--- Oregon Trail/src/testing/TestOregonTrail.java
+++ Oregon Trail/src/testing/TestOregonTrail.java
@@ -3,8 +3,20 @@ package testing;
 import static org.junit.Assert.*;
 
 import game.Map;
+import game.Store;
 import game.Wagon;
 import game.World;
+
+
+import items.Ammo;
+import items.Axle;
+import items.Clothing;
+import items.Food;
+import items.Medicine;
+import items.Oxen;
+import items.Tongue;
+import items.Water;
+import items.Wheel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +42,10 @@ public class TestOregonTrail {
 	private static List<Traveler> members;
 	private static Leader leader;
 	private static Map map;
+	Store R0Store;
+	Store R1Store;
+	Store R2Store;
+	Store R3Store;
 	
 	
 	/**
@@ -48,9 +64,17 @@ public class TestOregonTrail {
 		wagon.setLeader(leader);
 		wagon.setMembers(members);
 		map = new Map();
+		R0Store = new Store("testing 0", 500);
+		R1Store = new Store("testing 1", 800);
+		R2Store = new Store("testing 2", 1300);
+		R3Store = new Store("testing 3", 1700);
 	}
 
-	//test the values of the wagon at start
+	/**
+	 * This tests the wagon's initialization
+	 * 
+	 * @author Dev
+	 */
 	@Test
 	public void testInitialWagon() {
 		List<Person> testPassengers = new ArrayList<Person>();
@@ -81,6 +105,55 @@ public class TestOregonTrail {
 			assertTrue("Members have incorrect hunger.", testPassengers.get(x).getHunger() == 0);
 			assertTrue("Members have incorrect thirst.", testPassengers.get(x).getThirst() == 0);
 		}
+	}
+	/**
+	 * This tests that the store's available inventory is properly initialized based on distance from start.
+	 * 
+	 * @author Stephen Bentley
+	 */
+	@Test
+	public void testStoreInitialization(){
+		//region 0 with everything available
+		assertTrue("Store0 Inventory missing Food", R0Store.getInventory().contains(new Food()));
+		assertTrue("Store0 Inventory missing Ammo", R0Store.getInventory().contains(new Ammo()));
+		assertTrue("Store0 Inventory missing Water", R0Store.getInventory().contains(new Water()));
+		assertTrue("Store0 Inventory missing Medicine", R0Store.getInventory().contains(new Medicine()));
+		assertTrue("Store0 Inventory missing Axle", R0Store.getInventory().contains(new Axle()));
+		assertTrue("Store0 Inventory missing Clothing", R0Store.getInventory().contains(new Clothing()));
+		assertTrue("Store0 Inventory missing Oxen", R0Store.getInventory().contains(new Oxen()));
+		assertTrue("Store0 Inventory missing Tongue", R0Store.getInventory().contains(new Tongue()));
+		assertTrue("Store0 Inventory missing Wheel", R0Store.getInventory().contains(new Wheel()));
+		//region 1 with most things available
+		assertTrue("Store1 Inventory missing Food", R1Store.getInventory().contains(new Food()));
+		assertTrue("Store1 Inventory missing Ammo", R1Store.getInventory().contains(new Ammo()));
+		assertTrue("Store1 Inventory missing Water", R1Store.getInventory().contains(new Water()));
+		assertTrue("Store1 Inventory missing Medicine", R1Store.getInventory().contains(new Medicine()));
+		assertTrue("Store1 Inventory missing Axle", R1Store.getInventory().contains(new Axle()));
+		assertFalse("Store1 Inventory not missing Clothing", R1Store.getInventory().contains(new Clothing()));
+		assertTrue("Store1 Inventory missing Oxen", R1Store.getInventory().contains(new Oxen()));
+		assertTrue("Store1 Inventory missing Tongue", R1Store.getInventory().contains(new Tongue()));
+		assertFalse("Store1 Inventory not missing Wheel", R1Store.getInventory().contains(new Wheel()));
+		//region 2 with less available
+		assertTrue("Store2 Inventory missing Food", R2Store.getInventory().contains(new Food()));
+		assertTrue("Store2 Inventory missing Ammo", R2Store.getInventory().contains(new Ammo()));
+		assertFalse("Store2 Inventory not missing Water", R2Store.getInventory().contains(new Water()));
+		assertFalse("Store2 Inventory not missing Medicine", R2Store.getInventory().contains(new Medicine()));
+		assertTrue("Store2 Inventory missing Axle", R2Store.getInventory().contains(new Axle()));
+		assertTrue("Store2 Inventory missing Clothing", R2Store.getInventory().contains(new Clothing()));
+		assertFalse("Store2 Inventory not missing Oxen", R2Store.getInventory().contains(new Oxen()));
+		assertFalse("Store2 Inventory not missing Tongue", R2Store.getInventory().contains(new Tongue()));
+		assertTrue("Store2 Inventory missing Wheel", R2Store.getInventory().contains(new Wheel()));
+		//region 3 with almost nothing available
+		assertTrue("Store3 Inventory missing Food", R3Store.getInventory().contains(new Food()));
+		assertFalse("Store3 Inventory not missing Ammo", R3Store.getInventory().contains(new Ammo()));
+		assertFalse("Store3 Inventory not missing Water", R3Store.getInventory().contains(new Water()));
+		assertTrue("Store3 Inventory missing Medicine", R3Store.getInventory().contains(new Medicine()));
+		assertFalse("Store3 Inventory not missing Axle", R3Store.getInventory().contains(new Axle()));
+		assertFalse("Store3 Inventory not missing Clothing", R3Store.getInventory().contains(new Clothing()));
+		assertTrue("Store3 Inventory missing Oxen", R3Store.getInventory().contains(new Oxen()));
+		assertFalse("Store3 Inventory not missing Tongue", R3Store.getInventory().contains(new Tongue()));
+		assertFalse("Store3 Inventory not missing Wheel", R3Store.getInventory().contains(new Wheel()));
+		
 	}
 	
 	/**
