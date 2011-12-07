f85eeeaef368452380bd1fc67fd7e1616e8c7ffb
dbyassmith@gmail.com <dbyassmith@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-12-07 03:51:46 +0000

    updated graphics on win,lose, and river screen

--- Oregon Trail/src/gui/WinScreen.java
+++ Oregon Trail/src/gui/WinScreen.java
@@ -5,8 +5,12 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Canvas;
 
 /**
  * Screen that shows when the user wins the game
@@ -18,6 +22,9 @@ public class WinScreen extends Composite {
 	private Label lbl1;
 	private Button btnNewGame;
 	private Button btnQuitGame;
+	private Canvas canvas;
+	private Canvas canvas_1;
+	private Image image;
 	
 	/**
 	 * Create the composite.
@@ -72,12 +79,22 @@ public class WinScreen extends Composite {
 	 */
 	private void createContents(){
 		lbl1 = new Label(this, SWT.NONE);
-		lbl1.setFont(SWTResourceManager.getFont("Segoe UI", 40, SWT.NORMAL));
-		lbl1.setBounds(43, 50, 290, 82);
-		lbl1.setText("YOU WIN!");
+		lbl1.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
+		lbl1.setBounds(10, 240, 430, 25);
+		lbl1.setText("Congratulations! You have reached Oregon!! You Win!!");
 		
 		btnQuitGame = new Button(this, SWT.NONE);
-		btnQuitGame.setBounds(258, 180, 75, 25);
+		btnQuitGame.setBounds(181, 265, 75, 25);
 		btnQuitGame.setText("Quit");
+		
+		canvas = new Canvas(this, SWT.NONE);
+		canvas.setBounds(10, 10, 430, 216);
+		image = new Image(getDisplay(),getClass().getResourceAsStream("images/oregon.jpg"));
+		
+		canvas.addPaintListener(new PaintListener() {
+	        public void paintControl(PaintEvent e) {
+	         e.gc.drawImage(image,0,0);
+	        }
+	    });
 	}
 }
