b81e33707782892e39cca87879bc90eb1446b37a
dbyassmith@gmail.com <dbyassmith@gmail.com@1b33edee-8f94-8728-8c0c-dd74fa7a89c8>
2011-12-07 03:51:46 +0000

    updated graphics on win,lose, and river screen

--- trunk/Oregon Trail/src/gui/LoseScreen.java
+++ trunk/Oregon Trail/src/gui/LoseScreen.java
@@ -5,14 +5,20 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Canvas;
 
 public class LoseScreen extends Composite {
 	private boolean done = false;
 	private Label lbl1;
 	private Button btnNewGame;
 	private Button btnQuitGame;
+	private Canvas canvas;
+	private Image image;
 	/**
 	 * Create the composite.
 	 * @param parent
@@ -66,13 +72,28 @@ public class LoseScreen extends Composite {
 	 */
 	private void createContents(){
 		lbl1 = new Label(this, SWT.NONE);
-		lbl1.setFont(SWTResourceManager.getFont("Segoe UI", 29, SWT.NORMAL));
-		lbl1.setBounds(68, 72, 220, 46);
-		lbl1.setText("You lose!");
+		lbl1.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+		lbl1.setBounds(10, 234, 430, 25);
+		lbl1.setText("Sorry. You were not smart enough to survive the Oregon Trail.");
 		
 		btnQuitGame = new Button(this, SWT.NONE);
 		btnQuitGame.setEnabled(true);
-		btnQuitGame.setBounds(294, 247, 75, 25);
+		btnQuitGame.setBounds(365, 265, 75, 25);
 		btnQuitGame.setText("Quit");
+		
+		Label lblYouLose = new Label(this, SWT.NONE);
+		lblYouLose.setFont(SWTResourceManager.getFont("Segoe UI", 20, SWT.BOLD | SWT.ITALIC));
+		lblYouLose.setBounds(130, 252, 165, 35);
+		lblYouLose.setText("YOU LOSE!");
+		
+		canvas = new Canvas(this, SWT.NONE);
+		canvas.setBounds(10, 10, 430, 216);
+		image = new Image(getDisplay(),getClass().getResourceAsStream("images/lose.jpg"));
+		
+		canvas.addPaintListener(new PaintListener() {
+	        public void paintControl(PaintEvent e) {
+	         e.gc.drawImage(image,0,0);
+	        }
+	    });
 	}
 }
--- trunk/Oregon Trail/src/gui/RiverScreen.java
+++ trunk/Oregon Trail/src/gui/RiverScreen.java
@@ -7,8 +7,11 @@ import exceptions.InsufficientFundsException;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
@@ -21,13 +24,11 @@ import org.eclipse.swt.widgets.Combo;
  */
 public class RiverScreen extends Composite {
 	private boolean done = false;
-	private Canvas c;
+	private Canvas canvas;
 	private Label lblCross;
-	private Label lblChoice;
+	private Image image;
 	private Combo crossMethods;
-	private Label lblDescription;
 	private Button crossButton;
-	private Label lblName;
 	
 	/**
 	 * Create the composite.
@@ -65,27 +66,18 @@ public class RiverScreen extends Composite {
 	 */
 	private void createContents(){
 		
-		c = new Canvas(this, SWT.NONE);
-		c.setBounds(271, 10, 169, 255);
+		
+		
 		
 		lblCross = new Label(this, SWT.NONE);
 		lblCross.setFont(SWTResourceManager.getFont("Tahoma", 14, SWT.NORMAL));
-		lblCross.setBounds(24, 40, 231, 25);
-		lblCross.setText("we need to cross the river!");
-		
-		lblChoice = new Label(this, SWT.NONE);
-		lblChoice.setFont(SWTResourceManager.getFont("Tahoma", 14, SWT.NORMAL));
-		lblChoice.setBounds(24, 64, 222, 25);
-		lblChoice.setText("What we gonna do?!?");
+		lblCross.setBounds(20, 228, 430, 25);
+		lblCross.setText("We need to cross the river! What we gonna do?!");
 		
 		crossMethods = new Combo(this, SWT.READ_ONLY);
 		crossMethods.setItems(new String[] {"Take Ferry", "Ford", "Caulk"});
-		crossMethods.setBounds(80, 133, 92, 21);
+		crossMethods.setBounds(35, 264, 148, 21);
 		crossMethods.setText("Take Ferry");
-		
-		lblDescription= new Label(this, SWT.NONE);
-		lblDescription.setAlignment(SWT.CENTER);
-		lblDescription.setBounds(24, 160, 222, 57);
 		
 		crossButton = new Button(this, SWT.NONE);
 		crossButton.addSelectionListener(new SelectionAdapter() {
@@ -118,14 +110,17 @@ public class RiverScreen extends Composite {
 				}
 			}
 		});
-		crossButton.setBounds(21, 231, 231, 34);
+		crossButton.setBounds(209, 256, 231, 34);
 		crossButton.setText("CROSS");
 		
-		Label lblResult = new Label(this, SWT.NONE);
-		lblResult.setBounds(10, 271, 430, 19);
+		canvas = new Canvas(this, SWT.NONE);
+		canvas.setBounds(10, 10, 430, 216);
+		image = new Image(getDisplay(),getClass().getResourceAsStream("images/river.jpg"));
 		
-		lblName = new Label(this, SWT.NONE);
-		lblName.setFont(SWTResourceManager.getFont("Tahoma", 14, SWT.NORMAL));
-		lblName.setBounds(24, 10, 231, 34);
+		canvas.addPaintListener(new PaintListener() {
+	        public void paintControl(PaintEvent e) {
+	         e.gc.drawImage(image,0,0);
+	        }
+	    });
 	}
 }
